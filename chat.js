2ba069b2e81cdb0dba1c7a6251a18238f475b7c8
Diegoudg94 <89985051+Diegoudg94@users.noreply.github.com>
2021-11-16 09:10:30 -0600

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,13 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fd244d6920fc91564cf78e4/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
